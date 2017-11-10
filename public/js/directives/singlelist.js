a4c445afa62af13d83ecf324955762847dccbc12
Appackers <mhariharasundaram@gmail.com>
2017-11-11 01:19:42 +0530

    Dashboard html first commit

---
+++ public/js/directives/singlelist.js
@@ -0,0 +1,11 @@
+app.directive('singlelist', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			mylist:'='
+		},
+		templateUrl: 'js/directives/singlelist.html'
+	}
+})
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. 
