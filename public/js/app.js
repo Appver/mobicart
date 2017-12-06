4a39c6e968155f71cfc680ab246d587e93364279
Appackers <mhariharasundaram@gmail.com>
2017-12-06 09:03:18 +0530

    Router Concept

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('ToDoList', []);
+var app = angular.module('KaviyaMobiles', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: '/view/signin-page.html',
+    controller: 'SigninController'
+  })
+   .when('/dashboard', {
+    templateUrl: '/view/admin-dashboard.html',
+    controller: 'DashboardController'
+  });
+});
+
+app.controller('SigninController', function($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+})
+
+.controller('DashboardController', function($scope, $routeParams) {
+
+});
