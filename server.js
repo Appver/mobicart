eb126f3c8bab087567e13055c6980193052714ea
Arunkumar <arunkumar86@gmail.com>
2017-12-24 15:11:58 +0530

    Dataserver change

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 //Create Connection - Remote1
+
 const db = mysql.createConnection({
+	host : 'sql12.freesqldatabase.com',
+	user : 'sql12207175',
+	password : 'g5aN9Qj4Zu',
+	database : 'sql12207175'
+});
+
+/*const db = mysql.createConnection({
 	host : '127.0.0.1',
 	user : 'root',
 	password : '',
 	database : 'sql12207175'
-});
+});*/
 
 //Create Connection - Remote2
 //const db = mysql.createConnection({
